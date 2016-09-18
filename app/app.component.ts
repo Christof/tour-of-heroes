7c7ed22341c2dbb1571651bfc17bc00b4aa47839
christof <christof.sirk@gmail.com>
2016-09-18 20:20:00 +0200

    Introduce service for heroes (for now just using mock heroes).

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 
 @Component({
@@ -64,26 +65,27 @@ import { Hero } from './hero';
         border-radius: 4px 0px 0px 4px;
       }
     `
-  ]
+  ],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'Tour of Heroes';
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
   onSelect(hero: Hero) {
     this.selectedHero = hero;
   }
-  public heroes = HEROES;
+  heroes: Hero[];
 }
 
-var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-];
