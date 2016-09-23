b7d62fdd103af608d1ce99498023f7c0f8d3cabf
christof <christof.sirk@gmail.com>
2016-09-23 22:00:35 +0200

    Default route redirects to /dashboard.

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -12,8 +12,14 @@ const appRoutes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
   }
 ];
+
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
 
